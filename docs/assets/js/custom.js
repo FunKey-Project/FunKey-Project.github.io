31e10ec2fd44f21a21c3b318024f2fdcdd04943e
Michel-FK <michel.stempin@funkey-project.com>
2021-01-24 23:08:10 +0100

    test iFixIt-style assembly guide
    Signed-off-by: Michel-FK <michel.stempin@funkey-project.com>

--- docs/assets/js/custom.js
+++ docs/assets/js/custom.js
@@ -1,24 +1,45 @@
 $(document).ready(function () {
-  let imageGroups = []
-  $(".lightbox").each(function () {
-    let imageSource = $(this).attr('src')
-    let imageAlt = $(this).attr('alt').replace(/ /g,"")
-    let imageTitle = $(this).attr('alt')
-    if (imageTitle) {
-      imageTitle = 'title="' + imageTitle + '" '
-    }
-    else {
-      imageTitle = ''
-    }
-    $(this).
-        wrap('<a class="boxedThumb ' + imageAlt + '" ' +
-            imageTitle + 'href="' + imageSource + '"></a>')
-    imageGroups.push('.' + imageAlt)
-  })
-  jQuery.unique(imageGroups)
-  imageGroups.forEach(imageGroupsSet)
+    $(".step-container img:first-child").each(function () {
+	$(this)
+	    .clone()
+	    .removeClass("thumb")
+	    .addClass("large-image")
+	    .insertBefore($(this));
+	$(this).addClass("active");
+    })
 
-  function imageGroupsSet (value) {
-    $(value).simpleLightbox()
-  }
+    let imageGroups = []
+    $(".lightbox").each(function () {
+	let imageSource = $(this).attr('src')
+	let imageAlt = $(this).attr('alt').replace(/ /g,"")
+	let imageTitle = $(this).attr('alt')
+	if (imageTitle) {
+	    imageTitle = 'title="' + imageTitle + '" '
+	}
+	else {
+	    imageTitle = ''
+	}
+	$(this).
+            wrap('<a class="boxedThumb ' + imageAlt + '" ' +
+		 imageTitle + 'href="' + imageSource + '"></a>')
+	imageGroups.push('.' + imageAlt)
+    })
+    jQuery.unique(imageGroups)
+    imageGroups.forEach(imageGroupsSet)
+
+    function imageGroupsSet (value) {
+	$(value).simpleLightbox()
+    }
 })
+
+// Attach a delegate function on mouse over thumbnail
+$(".thumb").on("mouseover", function() {
+
+    // Set large image source to the thumbnail image source
+    $(this).siblings("img:first")
+	.attr("src", $(this).attr("src"));
+
+    // Activate the corresponding step-thumbnail and deactivate all others
+    $(this).addClass("active")
+	.siblings().removeClass("active");
+});
--- docs/tutorials/hardware/disassembling_console.md
+++ docs/tutorials/hardware/disassembling_console.md
@@ -1,3 +1,19 @@
+## Step 1
+<div class="step">
+    <div class="row step-container">
+        <img class="thumb" src="/assets/images/IMG_8800.jpg" />
+        <img class="thumb" src="/assets/images/IMG_8801.jpg" />
+        <img class="thumb" src="/assets/images/IMG_8802.jpg" />
+    </div>
+</div>
+
+## Step 2
+<div class="step">
+    <div class="row step-container">
+        <img class="thumb" src="/assets/images/IMG_8803.jpg" />
+        <img class="thumb" src="/assets/images/IMG_8804.jpg" />
+    </div>
+</div>
 
 --8<--
 includes/glossary.md
