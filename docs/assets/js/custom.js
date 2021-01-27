34cad980d0ca5dc4fc9e938feffac84491f52b87
Michel-FK <michel.stempin@funkey-project.com>
2021-01-28 00:11:06 +0100

    reintroduce lightbox, now with iFixIt steps
    Signed-off-by: Michel-FK <michel.stempin@funkey-project.com>

--- docs/assets/js/custom.js
+++ docs/assets/js/custom.js
@@ -95,11 +95,34 @@ $(document).ready(function () {
             .clone()
             .removeClass("step-thumb")
             .addClass("step-picture")
+	    .addClass("lightbox")
             .insertBefore($(this).parent());
 
         /* Make the first thumbnail active by default */
         $(this).addClass("active");
     })
+
+    let imageGroups = []
+    $(".lightbox").each(function () {
+	let imageSource = $(this).attr('src')
+	let imageAlt = $(this).attr('alt').replace(/ /g,"")
+	let imageTitle = $(this).attr('alt')
+	if (imageTitle) {
+	    imageTitle = 'title="' + imageTitle + '" '
+	} else {
+	    imageTitle = ''
+	}
+	$(this).
+            wrap('<a class="boxedThumb ' + imageAlt + '" ' +
+		 imageTitle + 'href="' + imageSource + '"></a>')
+	imageGroups.push('.' + imageAlt)
+    })
+    jQuery.unique(imageGroups)
+    imageGroups.forEach(imageGroupsSet)
+
+    function imageGroupsSet (value) {
+	$(value).simpleLightbox()
+    }
 })
   
 /* Attach a delegate function on mouse over thumbnail to change the
